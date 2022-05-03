30b77bb671dc1d9d58a7b70984da48b382b751fe
eedyGarcia <eedy.ega@gmail.com>
2022-05-03 00:12:16 -0500

    u2022050300

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
 import { ProgressbarsComponent } from './components/progressbars/progressbars.component';
+import { ActividadesComponent } from './components/actividades/actividades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BienvenidaComponent,
-    ProgressbarsComponent
+    ProgressbarsComponent,
+    ActividadesComponent
   ],
   imports: [
     BrowserModule,
